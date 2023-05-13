aa7375c1b0ea6ea0e2657ccd5479773c3343d836
sandesh-java-21 <85265264+sandesh-java-21@users.noreply.github.com>
2023-05-13 12:13:43 +0500

    changes in design

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -44,7 +44,7 @@ const PopUp = ({ onHide = () => {}, show, title, image }, props) => {
           style={{
             width: "100%",
             height: "100%",
-            objectFit: "cover",
+            objectFit: "contain",
           }}
           src={image}
           alt=""
